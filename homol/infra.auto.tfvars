6c23973c2724ef6224cb8ee85b524a3b2689ae0d
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2025-03-13 20:50:03 +0000

    test-create-stgw

---
+++ homol/infra.auto.tfvars
@@ -0,0 +1,12 @@
+################# value Variables #########################
+###########################################################
+instance_type    = "m6i.large"
+subnet_id        = []
+security_groups  = []
+gateway_name     = "gopoints-stgw-us-east-1"
+gateway_timezone = "GMT-3:00"
+gateway_type     = "FILE_S3"
+client_list      = ["0.0.0.0/0"]
+
+
+
--- homol/variables.tf
+++ homol/variables.tf
@@ -1,22 +1,69 @@
 ####################################
 ####### Tags Variables #############
 ####################################
-
-variable "environment"{
-    description = "Environment of the application"
-    type        = string
-    default     = "demo"
-}
-
 variable "provisioner" {
-    description = "IAC language type"
-    type        = string
-    default     = "Terraform"
+  description = "IAC language type"
+  type        = string
+  default     = "Terraform"
 }
 
 variable "repo" {
-    description = "Repository of the application"
-    type        = string
-    default     = "GitHub"
+  description = "Repository of the application"
+  type        = string
+  default     = "GitHub"
 }
 
+variable "Environment"{
+  description = "Environment of the application"
+  type        = string
+  default     = "Production"
+}
+
+
+variable "Customer"{
+  description   = "Environment of the application"
+  type          = string
+  default       = "GoPoints"
+}
+
+variable "instance_type" {
+  description   = "(Required) Shape Familia de máquina Ec2"
+  type          = string
+  
+}
+
+variable "subnet_id" {
+  description   = "Subnet's que serão utilizado no Ec2"
+  type          = list(string)  
+}
+
+variable "security_groups" {
+  description   = "SG que serão utilizado no Ec2"
+  type          = list(string)
+}
+
+variable "gateway_name" {
+ description    = "(Required) Nome do gateway"
+ type           = string
+  
+}
+
+variable "gateway_timezone" {
+    description = "(Required) Fuso horário para o gateway. O fuso horário é do formato GMT"
+    type        = string
+    default     = "GMT-3:00"
+  
+}
+
+variable "gateway_type" {
+    description = "Tipo do gateway"
+    type        = string
+    default     = "FILE_S3"
+  
+}
+
+variable "client_list" {
+  description   = "(Required) A lista de clientes que têm permissão para acessar o gateway de arquivo. A lista deve conter endereços IP válidos ou blocos CIDR válidos"
+  type          = list(string)
+  
+}
